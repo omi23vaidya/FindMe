90aebac23de290096534af0a8275c7fce46c8675
Omkar Vaidya <omi23.vaidya@gmail.com>
2018-04-07 20:12:09 -0400

    login and register page done with connection with server

--- Frontend/LoginAndroid3/app/src/main/java/com/example/omkarvaidya/loginandroid3/RegisterActivity.java
+++ Frontend/LoginAndroid3/app/src/main/java/com/example/omkarvaidya/loginandroid3/RegisterActivity.java
@@ -1,14 +1,11 @@
 package com.example.omkarvaidya.loginandroid3;
 
-import android.content.Context;
 import android.content.Intent;
-import android.content.SharedPreferences;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.CardView;
 import android.util.Log;
 import android.view.View;
-import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
@@ -20,35 +17,34 @@ import com.android.volley.VolleyError;
 import com.android.volley.toolbox.StringRequest;
 import com.android.volley.toolbox.Volley;
 
-import org.w3c.dom.Text;
+public class RegisterActivity extends AppCompatActivity {
 
-import static android.R.attr.button;
-
-public class MainActivity extends AppCompatActivity {
-
-    SharedPreferences sharedpreferences;
-    public static final String mypreference = "mypref";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-        sharedpreferences = getSharedPreferences(mypreference,
-                Context.MODE_PRIVATE);
+        setContentView(R.layout.activity_register);
 
-//        cardView = (CardView) findViewById(R.id.cardView);
-        final TextView etUname = (TextView) findViewById(R.id.textView);
-        final EditText etPass = (EditText) findViewById(R.id.editText);
-        final CardView btnReg = (CardView) findViewById(R.id.cardView);
-        final TextView register = (TextView) findViewById(R.id.textView3);
+        final TextView back = (TextView) findViewById(R.id.textView7);
+
+        back.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(getApplicationContext(), MainActivity.class));
+            }
+        });
+
+        final TextView regUname = (TextView) findViewById(R.id.textView4);
+        final EditText regPass = (EditText) findViewById(R.id.editText2);
+        final CardView btnReg = (CardView) findViewById(R.id.registerCardView);
 
         btnReg.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 // Request a string response from the provided URL.
                 RequestQueue queue = Volley.newRequestQueue(getApplicationContext());
-                final String uname=etUname.getText().toString();
-                String pass=etPass.getText().toString();
-                String url = "http://232e6d07.ngrok.io/login?username="+uname+"&pass="+pass;
+                String uname=regUname.getText().toString();
+                String pass=regPass.getText().toString();
+                String url = "http://232e6d07.ngrok.io/register?username="+uname+"&pass="+pass;
                 StringRequest postRequest = new StringRequest(Request.Method.GET, url,
                         new Response.Listener<String>()
                         {
@@ -59,21 +55,9 @@ public class MainActivity extends AppCompatActivity {
                                 {
                                     Toast.makeText(getBaseContext(),"Congrats! You can login",
                                             Toast.LENGTH_SHORT).show();
-                                    SharedPreferences.Editor editor = sharedpreferences.edit();
-                                    editor.putString("username", uname);
-                                    editor.commit();
-
-                                    sharedpreferences = getSharedPreferences(mypreference,
-                                            Context.MODE_PRIVATE);
-
-                                    if (sharedpreferences.contains("username")) {
-                                        Toast.makeText(getBaseContext(),(sharedpreferences.getString ("username", "##")),
-                                                Toast.LENGTH_SHORT).show();
-                                    }
-                                    else {
-                                        Toast.makeText(getBaseContext(),"User not available in SP",
-                                                Toast.LENGTH_SHORT).show();
-                                    }
+                                    //Intent myIntent = new Intent(MainActivity.this,
+                                    //      sharingPage.class);
+                                    //startActivity(myIntent);
                                 }
                                 else {
                                     Toast.makeText(getBaseContext(),"Sorry! Username is incorrect.",
@@ -94,16 +78,5 @@ public class MainActivity extends AppCompatActivity {
                 queue.add(postRequest);
             }
         });
-
-        register.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                //startActivity(new Intent(getApplicationContext(), RegisterActivity.class));
-                Intent registerPg = new Intent(getApplicationContext(),
-                RegisterActivity.class);
-                startActivity(registerPg);
-            }
-        });
     }
-
 }
